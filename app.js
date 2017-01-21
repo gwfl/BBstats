acc3797dd9ef5d56a20a90f1c726122a0f73fa5a
Lee <gwforumlakers@gmail.com>
2017-01-20 19:56:09 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-// Ionic Starter App, v0.9.20
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic'])
+angular.module('BBstatsH', ['ionic'])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
@@ -303,7 +296,7 @@ angular.module('starter', ['ionic'])
         "tf": 0
       }, {
         "Nm": "Gabe",
-        "Nu": "00",
+        "Nu": "13",
         "onc": false,
         "pp": 0,
         "pf": 0,
@@ -352,11 +345,12 @@ angular.module('starter', ['ionic'])
 
     if ($scope.undoCB) {
       undo = -1;
+      $rootScope.appLog = '-- ' + tt; //  
     } else {
       undo = 1;
+      $rootScope.appLog = '> ' + tt; //  
     }
 
-    $rootScope.appLog = '> ' + tt; //  
     if (xx < 0) {
       $scope.ttIdx = tt;
     } else {
