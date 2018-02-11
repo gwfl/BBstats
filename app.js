af19ebd01d8f705029bfc45c78786c3cd47643bb
Lee <gwforumlakers@gmail.com>
2018-02-11 14:24:47 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,11 @@
-angular.module('BBstatsH', ['ionic'])
+var ionApp = angular.module('BBstatsH', ['ionic', 'ngResource']);
 
-.config(function($stateProvider, $urlRouterProvider) {
+ionApp.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-
     .state('app', {
     url: "/app",
     abstract: true,
     templateUrl: "menu.html"
-  })
-
-  /* 
-      .state('app.search', {
-        url: "/search",
-        views: {
-          'menuContent' :{
-            templateUrl: "search.html"
-          }
-        }
-      })
-      .state('app.browse', {
-        url: "/browse",
-        views: {
-          'menuContent' :{
-            templateUrl: "browse.html"
-          }
-        }
-      })
-      .state('app.single', {
-        url: "/main/:playlistId",
-        views: {
-          'menuContent' :{
-            templateUrl: "playlist.html",
-            controller: 'PlaylistCtrl'
-          }
-        }
-      })
-  */
-
-  .state('app.roster', {
-    url: "/roster",
-    views: {
-      'menuContent' :{
-        templateUrl: "roster.html",
-    controller: 'AppCtrl'
-      }
-    }
   })
   .state('app.main', {
     url: "/main",
@@ -54,242 +15,103 @@ angular.module('BBstatsH', ['ionic'])
         controller: 'AppCtrl'
       }
     }
+  })
+  .state('app.roster', {
+    url: "/roster",
+    views: {
+      'menuContent': {
+        templateUrl: "roster.html",
+        controller: 'AppCtrl'
+      }
+    }
   });
-
-  // if none of the above states are matched, use this as the fallback
+    // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/main');
+});
 
-})
+ionApp.factory('dbSvc', function ($resource, $http) {
 
-.run(function($ionicPlatform, $rootScope) {
-    $ionicPlatform.ready(function() {
+  var _ngr = function () {
+    var url = 'https://gwfl-256d.restdb.io/rest/songlist/:id?apikey=5821f61550e9b39131fe1b6f';
+    return $resource(url);
+  };
+
+  var _initRoster = function () { 
+    $http.get('https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Players/recKbHjCbXLbJuSuJ?api_key=key66fQg5IghIIQmb')    // /recKbHjCbXLbJuSuJ
+      .success(function (jsonData) {
+        localStorage.setItem('ls_vGM00', JSON.stringify(jsonData.fields.vGMstats));
+    });
+  };
+
+  var _allSongs = function () {
+//    return $resource('https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Players?api_key=key66fQg5IghIIQmb');
+    return $resource('https://gwfl-256d.restdb.io/rest/songlist?apikey=5821f61550e9b39131fe1b6f');
+  };
+
+  var _scoreById = function () {
+    var url = 'https://gwfl-256d.restdb.io/rest/scores/:recId?apikey=5821f61550e9b39131fe1b6f';    //  5a6b9e9da07bee72000109a7 
+    return $resource(url,      
+    { recId: '@_id' }, 
+    { update: { method: 'PUT' } }
+  )};
+    
+  return {
+    initRoster: _initRoster(),
+    scoreById: _scoreById,
+    allSongs: _allSongs().query()
+  };
+});
+
+ionApp.run( function ($rootScope, $http, dbSvc, $ionicPlatform) {
+
+  $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       //  if(window.cordova && window.cordova.plugins.Keyboard) {
       //    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       //  }
-      if (window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-    });
-
-    function storageAvailable(type) {
-      try {
-        var storage = window[type],
-          x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.clear();
-        return true;
-      } catch (e) {
-        return false;
-      }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
     }
+  });
 
-    $rootScope.appLog = "Sorry, No Storage Here.";
-    if (storageAvailable('sessionStorage')) {
-      // Yippee! We can use localStorage awesomeness
-      $rootScope.appLog = " ";
+  function storageAvailable(type) {
+    try {
+      var storage = window[type],
+        x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.clear();
+      return true;
+    } catch (e) {
+      return false;
     }
+  }
 
-    $rootScope.vGm00 = {
-      vGH: {
-        pp: 0,
-        ff: 0,
-        tt: 0
-      },
-      vGV: {
-        pp: 0,
-        ff: 0,
-        tt: 0
-      },
-      vP: [  { Nm: "Isaiah", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 },
-      { Nm: "Zafer", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 }, 
-      { Nm: "Carlos", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 },  
-      { Nm: "Landen", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 },  
-      { Nm: "Mason", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 }, 
-      { Nm: "Yousuf", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 },
-      { Nm: "Connor", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 },
-      { Nm: "Aidan", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 },
-      { Nm: "Aakash", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 },
-      { Nm: "Zach", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 },
-      { Nm: "Bryan", Nu: 0, onc: false, pp: 0, pf: 0, rrfg: 0, rr3p: 0, rrft: 0, inG: 0, outG: 0, totPss: 0, y2p: 0, x2p: 0, y3p: 0, x3p: 0, yft: 0, xft: 0, ast: 0, stl: 0, drb: 0, orb: 0, tov: 0, blk: 0, tf: 0 }  ]
-    };
-    localStorage.setItem('vGm00', JSON.stringify($rootScope.vGm00));
+  $rootScope.appLog = "Sorry, No Storage Here.";
+  if (storageAvailable('localStorage')) {
+    // Yippee! We can use localStorage awesomeness
+    $rootScope.appLog = ".r0 ";
+  }
+  // localStorage.removeItem('ls_vGMstats');
+  dbSvc.initRoster;
+  // $rootScope.appLog += ".r" + localStorage.getItem('ls_vGM00');
 
-    if (localStorage.getItem('vGm') !== null) {
-      $rootScope.vGm = JSON.parse(localStorage.getItem('vGm'));
-    } else {
-      $rootScope.vGm = JSON.parse(localStorage.getItem('vGm00'));
-    }
+});      //  end .run
 
-    localStorage.setItem('vGm', JSON.stringify($rootScope.vGm));
-//    $rootScope.appLog += ".run";
+ionApp.controller('AppCtrl', function($scope, $rootScope, $http, $resource, $timeout) {
+
+var timer = function() {
+    $rootScope.appLog += ".t1 ";
+    // https://gwfl-256d.restdb.io/rest/scores/5a6b9e9da07bee72000109a7?apikey=5821f61550e9b39131fe1b6f
+    // https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Players/recKbHjCbXLbJuSuJ?api_key=key66fQg5IghIIQmb
+  $http.get('https://gwfl-256d.restdb.io/rest/scores/5a6b9e9da07bee72000109a7?apikey=5821f61550e9b39131fe1b6f') 
+    .success(function (jsonData) {
+      $scope.vGm = jsonData.vGMstats;
+  });
   
-  }) //  end .run
+  $timeout(timer, 5000);    // 1 second delay
+};
+        
+$timeout(timer, 250);  
 
-.controller('AppCtrl', function($scope, $rootScope, $http) {
-//  $rootScope.appLog += ".AppC";
-
-  $scope.tglundoCB = function() {
-    $scope.undoCB = !$scope.undoCB;
-  };
-
-  $scope.pTally = function(tt, xx) {
-
-    if ($scope.undoCB) {
-      undo = -1;
-      $rootScope.appLog = '-- ' + tt; //  
-    } else {
-      undo = 1;
-      $rootScope.appLog = '> ' + tt; //  
-    }
-
-    if (xx < 0) {
-      $scope.ttIdx = tt;
-    } else {
-      $rootScope.vGm.vP[xx].onc2 = false;
-      $scope.undoCB = false;
-      $scope.ttIdx = '.';
-      $rootScope.appLog +=  ' :' + $rootScope.vGm.vP[xx].Nu //  
-    }
-    
-    switch (tt) {
-      case 'y2p':
-        if (xx < 0) {
-          $rootScope.vGm.vGH.pp += 2 * undo;
-        } else {
-          $rootScope.vGm.vP[xx].pp += 2 * undo;
-          $rootScope.vGm.vP[xx].y2p += 1 * undo;
-          $rootScope.vGm.vP[xx].rrfg = Math.round(($rootScope.vGm.vP[xx].y2p / ($rootScope.vGm.vP[xx].x2p + $rootScope.vGm.vP[xx].y2p)) * 100);
-        }
-        break;
-      case 'x2p':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].x2p += 1 * undo;
-          $rootScope.vGm.vP[xx].rrfg = Math.round(($rootScope.vGm.vP[xx].y2p / ($rootScope.vGm.vP[xx].x2p + $rootScope.vGm.vP[xx].y2p)) * 100);
-        }
-        break;
-      case 'y3p':
-        if (xx < 0) {
-          $rootScope.vGm.vGH.pp += 3 * undo;
-        } else {
-          $rootScope.vGm.vP[xx].pp += 3 * undo;
-          $rootScope.vGm.vP[xx].y3p += 1 * undo;
-          $rootScope.vGm.vP[xx].rr3p = Math.round(($rootScope.vGm.vP[xx].y3p / ($rootScope.vGm.vP[xx].x3p + $rootScope.vGm.vP[xx].y3p)) * 100);
-        }
-        break;
-      case 'x3p':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].x3p += 1 * undo;
-          $rootScope.vGm.vP[xx].rr3p = Math.round(($rootScope.vGm.vP[xx].y3p / ($rootScope.vGm.vP[xx].x3p + $rootScope.vGm.vP[xx].y3p)) * 100);
-        }
-        break;
-      case 'yft':
-        if (xx < 0) {
-          $rootScope.vGm.vGH.pp += 1 * undo;
-        } else {
-          $rootScope.vGm.vP[xx].pp += 1 * undo;
-          $rootScope.vGm.vP[xx].yft += 1 * undo;
-          $rootScope.vGm.vP[xx].rrft = Math.round(($rootScope.vGm.vP[xx].yft / ($rootScope.vGm.vP[xx].xft + $rootScope.vGm.vP[xx].yft)) * 100);
-        }
-        break;
-      case 'xft':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].xft += 1 * undo;
-          $rootScope.vGm.vP[xx].rrft = Math.round(($rootScope.vGm.vP[xx].yft / ($rootScope.vGm.vP[xx].xft + $rootScope.vGm.vP[xx].yft)) * 100);
-        }
-        break;
-      case 'ast':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].ast += 1 * undo;
-        }
-        break;
-      case 'stl':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].stl += 1 * undo;
-        }
-        break;
-      case 'blk':
-        if (xx >= 0) {
-          $rootScope.vGm.vP[xx].blk += 1 * undo;
-        }
-        break;
-      case 'drb':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].drb += 1 * undo;
-        }
-        break;
-      case 'orb':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].orb += 1 * undo;
-        }
-        break;
-      case 'tov':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].tov += 1 * undo;
-        }
-        break;
-      case 'pf':
-        if (xx < 0) {
-          $rootScope.vGm.vGH.ff += 1 * undo;
-        } else {
-          $rootScope.vGm.vP[xx].pf += 1 * undo;
-        }
-        break;
-      case 'tf':
-        if (xx < 0) {
-          // $rootScope.vGm.vGH.pp += 0 * undo; 
-        } else {
-          $rootScope.vGm.vP[xx].tf += 1 * undo;
-        }
-        break;
-      case 'v2p':
-        $rootScope.vGm.vGV.pp += 2 * undo;
-        $scope.undoCB = false;
-        break;
-      case 'v3p':
-        $rootScope.vGm.vGV.pp += 3 * undo;
-        $scope.undoCB = false;
-        break;
-      case 'vft':
-        $rootScope.vGm.vGV.pp += 1 * undo;
-        $scope.undoCB = false;
-        break;
-      case 'vpf':
-        $rootScope.vGm.vGV.ff += 1 * undo;
-        $scope.undoCB = false;
-        break;
-      default:
-        $rootScope.appLog = ">";
-        break;
-    }
-
-    localStorage.setItem('vGm', JSON.stringify($rootScope.vGm));
-   var dNow = JSON.stringify($rootScope.vGm);
- $scope.data =   JSON.stringify({"name":"gwfLEEdb!!","rawScore":10,"vGMstats":dNow,"type":"rbyb-2" });
-$http.put('https://gwfl-256d.restdb.io/rest/scores/5a6b9e9da07bee72000109a7?apikey=5821f61550e9b39131fe1b6f', $scope.data);
-  };
-  
-  $scope.resetLS = function() {
-    localStorage.removeItem('vGm'); // clear();   
-    $rootScope.vGm = JSON.parse(localStorage.getItem('vGm00'));
-  };
-
-  
 });
