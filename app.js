f3dd791e6d4629d84f57e40dd2f30a9bceb656d2
Lee <gwforumlakers@gmail.com>
2020-04-18 19:38:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -31,36 +31,7 @@ ionApp.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/app/main');
 });
 
-ionApp.run( function ($rootScope, $http, dbSvc, $ionicPlatform) {
-
-  $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      //  if(window.cordova && window.cordova.plugins.Keyboard) {
-      //    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      //  }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-
-  function storageAvailable(type) {
-    try {
-      var storage = window[type],
-        x = '__storage_test__';
-      storage.setItem(x, x);
-      storage.clear();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  $rootScope.appLog = "Sorry, No Storage Here.";
-  if (storageAvailable('localStorage')) {
-    // Yippee! We can use localStorage awesomeness
-    $rootScope.appLog = ".r0 ";
-  }
+ionApp.run( function ($rootScope, $http, $ionicPlatform) {
 
   $rootScope.accGroup = [];  // grp00={ name: "__", isShown: false };
 //  for (hh = 0; hh < 20; hh++) {        
